42d9754550ffb6271ce89b87f50e62ab87e54634
Kuan Fang <kuanfang@outlook.com>
2019-07-12 15:42:51 -0700

    Depricate unnecessary files.

--- robovat/envs/__init__.py
+++ robovat/envs/__init__.py
@@ -1,13 +1,1 @@
-from .circle_env import CircleEnv
-from .brownian_env import BrownianEnv
 from .grasp_4dof_env import Grasp4DofEnv
-from .mjolnir_env import MjolnirEnv
-from .polyline_env import PolylineEnv
-from .manip_env import ManipEnv
-from .push_env import PushEnv
-
-from .push_env_v2 import PushEnvV2
-from .push_env_v2 import DataCollectionEnv
-from .push_env_v2 import GatherEnv
-from .push_env_v2 import InsertEnv
-from .push_env_v2 import CrossEnv
